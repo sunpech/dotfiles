f27a2d64c06eedd0b29a2509ad4548b9dbf95ffc
Steven Suwatanapongched <sunpech@users.noreply.github.com>
2024-10-05 20:21:33 -0500

    Added comments on favorite color schemes for wezterm. Changed to Hacktober.

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -10,8 +10,15 @@ local config = wezterm.config_builder()
 config.font = wezterm.font("MesloLGS Nerd Font Mono")
 config.font_size = 14.0
 
--- For example, changing the color scheme:
-config.color_scheme = "Github Dark"
+-- Color Scheme (favorites)
+-- Atom, Batman, Blue Matrix, , Classic Dark (base16), Dark Ocean (terminal.sexy), Dark+, darkmatrix, darkermatrix
+-- Default (dark) (terminal.sexy)
+-- Github Dark, Github Dark (Gogh), Google Dark (base16)
+-- Hacktober, MaterialDark, MaterialDarker, MaterialOcean, matrix
+-- Seti, Spiderman, The Hulk, Tokyo Night, Tokyo Night Moon, Twilight
+-- Twilight (dark) (terminal.sexy)
+-- Vs Code Dark+ (Gogh)
+config.color_scheme = 'Hacktober'
 
 -- and finally, return the configuration to wezterm
 return config
