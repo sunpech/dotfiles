f5899ff1cbada413edae5b3813a0aa20abe54759
Steven Suwatanapongched <sunpech@users.noreply.github.com>
2025-03-18 09:33:17 -0500

    Changed color scheme to Tokyo Night.

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -21,7 +21,7 @@ config.underline_thickness = '200%'
 -- Seti, Spiderman, The Hulk, Tokyo Night, Tokyo Night Moon, Twilight
 -- Twilight (dark) (terminal.sexy)
 -- Vs Code Dark+ (Gogh)
-config.color_scheme = "Seti"
+config.color_scheme = "Tokyo Night"
 
 -- Misc
 -- config.hide_tab_bar_if_only_one_tab = true
